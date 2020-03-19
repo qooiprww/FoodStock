641460ab7b58f4d36ade79f015dd0c9fac41b947
Haoran Liu <haoranliu@ibm.com>
2020-03-19 16:04:13 -0400

    init vue

---
+++ FoodPortal/app/main.js
@@ -0,0 +1,24 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+
+import store from './store'
+import VueDevtools from 'nativescript-vue-devtools'
+
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
+  
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+// Prints Colored logs when --env.production is *NOT* set while building
+Vue.config.debug = (TNS_ENV !== 'production')
+
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
+
+new Vue({
+  store,
+  render: h => h('frame', [h(App)])
+}).$start()
